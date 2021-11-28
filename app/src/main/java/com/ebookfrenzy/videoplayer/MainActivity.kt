25fd513e0aafe507e484bfe5872bf833ee7a638e
Yannick Loic <ngapmennyamiyannickloic@gmail.com>
2021-11-28 16:31:51 +0100

    Initial commit

---
+++ app/src/main/java/com/ebookfrenzy/videoplayer/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.ebookfrenzy.videoplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.net.Uri
+import android.widget.MediaController
+import android.util.Log
+
+import com.ebookfrenzy.videoplayer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var mediaController: MediaController? = null
+    private var TAG = "VideoPlayer"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        configureVideoView()
+    }
+
+    private fun configureVideoView() {
+
+        binding.videoView1.setVideoURI(Uri.parse("android.resource://"
+                + getPackageName() + "/" + R.raw.movie))
+
+        mediaController = MediaController(this)
+        mediaController?.setAnchorView(binding.videoView1)
+        binding.videoView1.setMediaController(mediaController)
+
+        binding.videoView1.setOnPreparedListener { mp ->
+            mp.isLooping = true
+            Log.i(TAG, "Duration = " + binding.videoView1.duration)
+        }
+        binding.videoView1.start()
+    }
+}
